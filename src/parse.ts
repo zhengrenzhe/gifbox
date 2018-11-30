829d951bfa2d7f28a113fa03aa50c5b4f701ea88
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2018-12-01 00:58:30 +0800

    makeup

--- src/parse.ts
+++ src/parse.ts
@@ -1,50 +1,33 @@
 import { splitByte, Uint8ToUint16 } from "./binary";
 
-function isGIF(signature: Uint8Array) {
-    return (
-        signature.length === 3 &&
-        signature[0] === 71 &&
-        signature[1] === 73 &&
-        signature[2] === 70
-    );
+export default function parse(buffer: Uint8Array) {
+    // GIF Signature 6 bytes(gif87a, gif89a)
+    const signature = getSignature(buffer);
+    if (signature !== "GIF87a" && signature !== "GIF89a")
+        throw new Error("Error: unrecognized gif version.");
+
+    // Logical Screen Descriptor 7 bytes(gif87a, gif89a)
+    const screenDescriptor = getScreenDescriptor(buffer);
+    console.log(screenDescriptor);
 }
 
-function getVersion(version: Uint8Array) {
-    if (version[1] === 57) return "89a";
-    if (version[1] === 55) return "87a";
-    throw new Error("Error: unrecognized gif version.");
+function getSignature(buffer: Uint8Array): GIF {
+    return Array.from(buffer.slice(0, 6))
+        .map(b => String.fromCharCode(b))
+        .join("") as GIF;
 }
 
-function parse(buffer: Uint8Array) {
-    // Header 6 bytes
-    const signature = buffer.slice(0, 3);
-    const version = getVersion(buffer.slice(3, 6));
-
-    if (!isGIF(signature)) throw new Error("Error: not gif.");
-
-    // Logical Screen Descriptor 7 bytes
-    const picWidth = Uint8ToUint16(buffer.slice(6, 8));
-    const picHeight = Uint8ToUint16(buffer.slice(8, 10));
-    const [
-        globalTableFlag,
-        colorResolution,
-        globalTableSorted,
-        globalTableSize,
-    ] = splitByte(buffer[10], [1, 3, 1, 3]);
+function getScreenDescriptor(buffer: Uint8Array): LogicalScreenDescriptor87 {
+    const width = Uint8ToUint16(buffer.slice(6, 8));
+    const height = Uint8ToUint16(buffer.slice(8, 10));
+    const [M, cr, _, pixle] = splitByte(buffer[10], [1, 3, 1, 3]);
     const backgroundColorIndex = buffer[11];
-    const pixelAspectRatio = buffer[12] === 0 ? 0 : (buffer[12] + 15) / 64;
-
-    // Global Color Table
-    let actualGlobalTableBytesCount = 0;
-    const globalTable: { [key: string]: Uint8Array } = {};
-    if (globalTableFlag === 1) {
-        actualGlobalTableBytesCount = 3 * Math.pow(2, globalTableSize + 1);
-        for (let i = 0; i < actualGlobalTableBytesCount; i += 3) {
-            const v = i / 3;
-            globalTable[`${v}${v}${v}`] = buffer.slice(13 + i, 13 + i + 3);
-        }
-    }
-    console.log(globalTable);
+    return {
+        screenWidth: width,
+        screenHeight: height,
+        hasGlobalColorTable: M === 1,
+        colorResolution: cr + 1,
+        pixle: pixle + 1,
+        backgroundColorIndex,
+    };
 }
-
-export default parse;
---
+++ src/types.d.ts
@@ -0,0 +1,20 @@
+type GIF = "GIF87a" | "GIF89a";
+
+type LogicalScreenDescriptor87 = {
+    screenWidth: number;
+    screenHeight: number;
+
+    /**
+     * the maximum number of colors within an image.
+     */
+    pixle: number;
+
+    hasGlobalColorTable: boolean;
+
+    /**
+     * bits of color resolution
+     */
+    colorResolution: number;
+
+    backgroundColorIndex: number;
+};
