5357239a13f07bfe392e6e26c4f513fa652bdc5a
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2018-12-02 16:25:08 +0800

    color table

--- src/parse.ts
+++ src/parse.ts
@@ -1,14 +1,22 @@
 import { splitByte, Uint8ToUint16 } from "./binary";
 
 export default function parse(buffer: Uint8Array) {
-    // GIF Signature 6 bytes(gif87a, gif89a)
+    // GIF Signature 6 bytes (gif87a, gif89a)
     const signature = getSignature(buffer);
     if (signature !== "GIF87a" && signature !== "GIF89a")
         throw new Error("Error: unrecognized gif version.");
 
-    // Logical Screen Descriptor 7 bytes(gif87a, gif89a)
+    // Logical Screen Descriptor 7 bytes (gif87a, gif89a)
     const screenDescriptor = getScreenDescriptor(buffer);
-    console.log(screenDescriptor);
+
+    // Global Color Table (gif87a, gif89a)
+    const GlobalColorTable = getGlobalColorTable(
+        screenDescriptor.hasGlobalColorTable,
+        screenDescriptor.pixle,
+        screenDescriptor.colorResolution,
+        buffer,
+    );
+    console.log(GlobalColorTable);
 }
 
 function getSignature(buffer: Uint8Array): GIF {
@@ -30,4 +38,26 @@ function getScreenDescriptor(buffer: Uint8Array): LogicalScreenDescriptor87 {
         pixle: pixle + 1,
         backgroundColorIndex,
     };
+}
+
+function getGlobalColorTable(
+    hasGlobalColorTable: boolean,
+    size: number,
+    colorResolution: number,
+    buffer: Uint8Array,
+): Uint8Array[] {
+    if (!hasGlobalColorTable) return [];
+
+    const table: Uint8Array[] = [];
+    const total = Math.pow(2, size) * 3;
+    for (let offset = 0; offset < total; offset += 3) {
+        let slice = buffer.slice(13 + offset, 13 + offset + 3);
+        if (colorResolution !== 8)
+            slice = slice.map(
+                b => (b * 255) / Math.pow(2, colorResolution - 1),
+            );
+        table.push(slice);
+    }
+
+    return table;
 }
